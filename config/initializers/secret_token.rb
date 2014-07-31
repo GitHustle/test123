07a873c2742bd190d7d18f4e22804d433c654029
GitHustle <abdoulhustles@gmail.com>
2014-07-31 11:33:43 -0400

    Initialize repository on GitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unemail::Application.config.secret_key_base = '9beefa41e646e927221c0a45f75a4e26540f73dbbde79bc72d6c1c3e3c0f52583f0d650052044e7153dd999ce1c32830caca65152ef4a0c5af6444c2fee57dee'
